40b69cdb022422ed97348c5c435654a79833ad7c
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-08-16 18:12:22 -0500

    Create IPokemonType and PokemonTypeUtils
    New interface 'IPokemonType' for calculating Pokémon Type Matchups regardless of Server/Client implementations. PokemonTypeUtils contains static functions for calculating damage multipliers based on a defender/attacker input.

--- PMDDesktop.Server.Assets/Data/PokemonType.cs
+++ PMDDesktop.Server.Assets/Data/PokemonType.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections.Immutable;
+﻿using PMDDesktop.GameData;
+using System.Collections.Immutable;
 using System.Text.Json.Serialization;
 
 namespace PMDDesktop.Server.Assets.Data;
 
 [AssetFileName("type")]
-public class PokemonType : Asset
+public class PokemonType : Asset, IPokemonType
 {
 
 	internal static AssetLocation DefaultTypeLocation(string typeName)
@@ -15,6 +16,21 @@ public class PokemonType : Asset
 	public ImmutableArray<AssetReference<PokemonType>> Weaknesses { get; internal set; } = [];
 	public ImmutableArray<AssetReference<PokemonType>> Resistances { get; internal set; } = [];
 	public ImmutableArray<AssetReference<PokemonType>> Immunities { get; internal set; } = [];
+
+	IEnumerable<IPokemonType> IPokemonType.Weaknesses =>
+		Weaknesses.Select((reference) =>
+			reference.GetReference(Manager ?? throw new NullReferenceException($"Can't resolve references of Weaknesses of {this} because Manager is null.")
+		));
+
+	IEnumerable<IPokemonType> IPokemonType.Resistances =>
+		Resistances.Select((reference) =>
+			reference.GetReference(Manager ?? throw new NullReferenceException($"Can't resolve references of Resistances of {this} because Manager is null.")
+		));
+
+	IEnumerable<IPokemonType> IPokemonType.Immunities =>
+		Resistances.Select((reference) =>
+			reference.GetReference(Manager ?? throw new NullReferenceException($"Can't resolve references of Immunities of {this} because Manager is null.")
+		));
 
 	[JsonConstructor]
 	private PokemonType() : this(new()) { }
---
+++ PMDDesktop/Utils/PokemonTypeUtils.cs
@@ -0,0 +1,84 @@
+﻿using PMDDesktop.GameData;
+
+namespace PMDDesktop.Utils;
+
+public static class PokemonTypeUtils
+{
+
+	public static readonly float INITIAL_MULTIPLIER = 1.0f;
+	public static readonly float WEAKNESS_MULTIPLIER = 2.0f;
+	public static readonly float RESISTANCE_MULTIPLIER = 0.5f;
+	public static readonly float IMMUNITY_MULTIPLIER = 0.0f;
+
+	public static bool IsWeakTo(this IPokemonType defender, IPokemonType attacker)
+	{
+
+		return defender.Weaknesses.Contains(attacker);
+
+	}
+
+	public static bool IsResistantTo(this IPokemonType defender, IPokemonType attacker)
+	{
+
+		return defender.Resistances.Contains(attacker);
+
+	}
+
+	public static bool IsImmuneTo(this IPokemonType defender, IPokemonType attacker)
+	{
+
+		return defender.Immunities.Contains(attacker);
+
+	}
+
+	public static float GetDamageMultiplier(IEnumerable<IPokemonType> defendingTypes, IEnumerable<IPokemonType> attackingTypes)
+	{
+
+		float total = INITIAL_MULTIPLIER;
+
+		foreach (IPokemonType attacker in attackingTypes)
+			total *= GetDamageMultiplier(defendingTypes, attacker);
+
+		return total;
+
+	}
+
+	public static float GetDamageMultiplier(IEnumerable<IPokemonType> defendingTypes, IPokemonType attackingType)
+	{
+
+		float total = INITIAL_MULTIPLIER;
+
+		foreach (IPokemonType defender in defendingTypes)
+			total *= GetDamageMultiplier(defender, attackingType);
+
+		return total;
+
+	}
+
+	public static float GetDamageMultiplier(IPokemonType defendingType, IEnumerable<IPokemonType> attackingTypes)
+	{
+
+		float total = INITIAL_MULTIPLIER;
+
+		foreach (IPokemonType attacker in attackingTypes)
+			total *= GetDamageMultiplier(defendingType, attacker);
+
+		return total;
+
+	}
+
+	public static float GetDamageMultiplier(IPokemonType defendingType, IPokemonType attackingType)
+	{
+
+		if (defendingType.IsWeakTo(attackingType))
+			return WEAKNESS_MULTIPLIER;
+		else if (defendingType.IsResistantTo(attackingType))
+			return RESISTANCE_MULTIPLIER;
+		else if (defendingType.IsImmuneTo(attackingType))
+			return IMMUNITY_MULTIPLIER;
+		else
+			return INITIAL_MULTIPLIER;
+
+	}
+
+}
