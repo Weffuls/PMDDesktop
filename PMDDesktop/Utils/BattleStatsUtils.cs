a17339b1988f38b8ba7cd2d4afea61667f5a7dca
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-08-17 00:02:07 -0500

    Added Stat Modifier calculator to BattleStatsUtils

--- PMDDesktop/Utils/BattleStatsUtils.cs
+++ PMDDesktop/Utils/BattleStatsUtils.cs
@@ -5,6 +5,8 @@ namespace PMDDesktop.Utils;
 public static class BattleStatsUtils
 {
 
+	public static readonly float STANDARD_STAT_MODIFIER_BASE = 2.0f;
+
 	public static BattleStats ToBattleStats(this IBattleStats source)
 	{
 
@@ -16,6 +18,25 @@ public static class BattleStatsUtils
 	{
 
 		return new(source);
+
+	}
+
+	public static uint ApplyStatModifier(uint originStat, int boostLevel)
+	{
+
+		float top = STANDARD_STAT_MODIFIER_BASE;
+		float bottom = STANDARD_STAT_MODIFIER_BASE;
+
+		if (boostLevel >= 0)
+			top += boostLevel;
+		else
+			bottom -= boostLevel; // Boost Level should be negative here.
+
+		float multiplier = (float)top / bottom;
+
+		uint calculated = (uint)multiplier * originStat;
+
+		return calculated == 0 ? 1 : calculated;
 
 	}
 
