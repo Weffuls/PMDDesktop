6ad9a9806df0ab8228bf5f425b6df9474d72c850
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-09-04 04:59:51 -0500

    Basic Build Unit Testing

---
+++ PMDDesktop.Server.Assets.Tests/Builder/BuildSpeciesTests.cs
@@ -0,0 +1,37 @@
+﻿using PMDDesktop.Server.Assets.Data;
+
+namespace PMDDesktop.Server.Assets.Tests.Builder;
+
+public class BuildSpeciesTests(BuildSpeciesFixture fixture) : IClassFixture<BuildSpeciesFixture>
+{
+
+	private BuildSpeciesFixture fixture = fixture;
+
+	[Fact]
+	public void CanReadMockZip()
+	{
+
+		Assert.NotNull(fixture.apiZip);
+		Assert.NotNull(fixture.spriteZip);
+
+	}
+
+	[Fact]
+	public void ContainsAtLeastOneSpecies()
+	{
+		Assert.NotEmpty(fixture.assets.OfType<Species>());
+	}
+
+	[Fact]
+	public void ContainsAtLeastOneSpeciesVariant()
+	{
+		Assert.NotEmpty(fixture.assets.OfType<Species>());
+	}
+
+	[Fact]
+	public void ContainsAtLeastOneSpeciesForm()
+	{
+		Assert.NotEmpty(fixture.assets.OfType<Species>());
+	}
+
+}
