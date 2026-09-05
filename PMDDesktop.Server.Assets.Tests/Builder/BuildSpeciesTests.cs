c059b5dd4741fc04190d28bf0b8977de96ed3ca9
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-09-04 23:02:06 -0500

    Add tests for linked data
    Tests like, do all variants have a base form; are all forms referenced by at least one variant. etc. etc.

--- PMDDesktop.Server.Assets.Tests/Builder/BuildSpeciesTests.cs
+++ PMDDesktop.Server.Assets.Tests/Builder/BuildSpeciesTests.cs
@@ -7,6 +7,8 @@ public class BuildSpeciesTests(BuildSpeciesFixture fixture) : IClassFixture<Buil
 
 	private BuildSpeciesFixture fixture = fixture;
 
+	#region Existence
+
 	[Fact]
 	public void CanReadMockZip()
 	{
@@ -33,5 +35,57 @@ public class BuildSpeciesTests(BuildSpeciesFixture fixture) : IClassFixture<Buil
 	{
 		Assert.NotEmpty(fixture.assets.OfType<Species>());
 	}
+
+	#endregion Existence
+
+	#region Linked Data
+
+	[Fact]
+	public void AllSpeciesHaveAVariant()
+	{
+
+		IEnumerable<SpeciesVariant> variants = fixture.assets.OfType<SpeciesVariant>();
+
+		Assert.All(fixture.assets.OfType<Species>(), (species) => Assert.Contains(variants, (variant) => variant.Species.GetReference(fixture.assets) == species));
+
+	}
+
+	[Fact]
+	public void AllVariantsHaveASpecies()
+	{
+
+		Assert.All(fixture.assets.OfType<SpeciesVariant>(), (variant) => Assert.NotNull(variant.Species.GetReference(fixture.assets)));
+
+	}
+
+	[Fact]
+	public void AllVariantsHaveADefaultForm()
+	{
+
+		Assert.All(fixture.assets.OfType<SpeciesVariant>(), (variant) => Assert.NotNull(variant.DefaultForm.GetReference(fixture.assets)));
+
+	}
+
+	[Fact]
+	public void AllFormsHaveAVariant()
+	{
+
+		IEnumerable<SpeciesVariant> variants = fixture.assets.OfType<SpeciesVariant>();
+
+		Assert.All(fixture.assets.OfType<SpeciesForm>(), (form) => Assert.Contains(variants, (variant) => DoesVariantReferenceForm(variant, form)));
+
+	}
+
+	private bool DoesVariantReferenceForm(SpeciesVariant variant, SpeciesForm form)
+	{
+
+		if (variant.DefaultForm.GetReference(fixture.assets) == form)
+			return true;
+
+		return variant.OtherForms.Any((reference) => reference.GetReference(fixture.assets) == form);
+
+	}
+
+	#endregion Linked Data
 
 }
