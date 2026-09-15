e1135a8d0b698f019eb0616f0b7dcc8c180108ba
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-09-15 08:55:40 -0500

    Add Monogame Base

---
+++ PMDDesktop.Client/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PMDDesktop.Client;
+
+internal static class Program
+{
+
+	private static void Main(string[] args)
+	{
+
+		using var game = new PMDDesktopGame();
+		game.Run();
+
+	}
+
+}
