80bfa8071ccda4bebd30a621d8a6b53392dbd421
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-09-15 19:02:46 -0500

    Add ContentPool and WonderMail font.

---
+++ PMDDesktop.Client/ContentPool.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace PMDDesktop.Client;
+
+public class ContentPool
+{
+
+	internal ContentPool(ContentManager contentManager)
+	{
+
+		// TODO: Make this credit more clear; somewhere in the Client UI.
+		WonderMailFont = contentManager.Load<SpriteFont>("Fonts/WonderMail"); // Wonder Mail font by ShinxHijinx
+
+	}
+
+	private ContentManager contentManager;
+
+	public SpriteFont WonderMailFont { get; init; }
+
+}
--- PMDDesktop.Client/PMDDesktopGame.cs
+++ PMDDesktop.Client/PMDDesktopGame.cs
@@ -1,50 +1,61 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 
 namespace PMDDesktop.Client;
 
 public class PMDDesktopGame : Game
 {
-	private GraphicsDeviceManager _graphics;
-	private SpriteBatch _spriteBatch;
+	private GraphicsDeviceManager graphics;
+	private SpriteBatch spriteBatch;
+	public ContentPool ContentPool { get; private set; }
 
 	public PMDDesktopGame()
 	{
-		_graphics = new GraphicsDeviceManager(this);
+		graphics = new GraphicsDeviceManager(this);
+		Content.RootDirectory = "Content";
 		IsMouseVisible = true;
 	}
 
 	protected override void Initialize()
 	{
+
 		// TODO: Add your initialization logic here
 
 		base.Initialize();
+
 	}
 
 	protected override void LoadContent()
 	{
-		_spriteBatch = new SpriteBatch(GraphicsDevice);
 
-		// TODO: use this.Content to load your game content here
+		spriteBatch = new SpriteBatch(GraphicsDevice);
+
+		ContentPool = new ContentPool(Content);
+
 	}
 
 	protected override void Update(GameTime gameTime)
 	{
-		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-			Exit();
 
 		// TODO: Add your update logic here
 
 		base.Update(gameTime);
+
 	}
 
 	protected override void Draw(GameTime gameTime)
 	{
+
 		GraphicsDevice.Clear(Color.CornflowerBlue);
 
-		// TODO: Add your drawing code here
+		spriteBatch.Begin();
+
+		spriteBatch.DrawString(ContentPool.WonderMailFont, "Weffuls", Vector2.One, Color.White);
+
+		spriteBatch.End();
 
 		base.Draw(gameTime);
+
 	}
+
 }
