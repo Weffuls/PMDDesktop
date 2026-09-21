55ecf969e1eb17db8d7b923d406cdc1673682c9a
Weffuls <111707520+Weffuls@users.noreply.github.com>
2026-09-21 15:53:01 -0500

    Add UserReferenceTests

---
+++ PMDDesktop.Server.Users.Tests/UserReferenceTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace PMDDesktop.Server.Users.Tests;
+
+public class UserReferenceTests
+{
+
+	public static readonly UserCreationOptions DEFAULT_USER_OPTIONS = new()
+	{
+		DisplayName = "Test User",
+		LoginHandle = "test",
+		PlainTextPassword = "Test123"
+	};
+
+	[Fact]
+	public async Task CanGetReference()
+	{
+
+		UserManager manager = new();
+		User? user = await manager.TryCreateUser(DEFAULT_USER_OPTIONS);
+		Assert.NotNull(user);
+
+		UserReference reference = new(user);
+
+		reference.TryGetUser(manager, out User? referUser);
+
+		Assert.NotNull(referUser);
+		Assert.Equal(referUser, user);
+
+	}
+
+	[Fact]
+	public async Task SerializationIsSimple()
+	{
+
+		UserManager manager = new();
+		User? user = await manager.TryCreateUser(DEFAULT_USER_OPTIONS);
+		Assert.NotNull(user);
+
+		UserReference preReference = new(user);
+
+		JsonElement json = JsonSerializer.SerializeToElement(preReference, AppInfo.JSON_OPTIONS);
+
+		Assert.Equal(JsonValueKind.String, json.ValueKind);
+
+		UserReference? postReference = JsonSerializer.Deserialize<UserReference>(json);
+
+		Assert.NotNull(postReference);
+
+		postReference.TryGetUser(manager, out User? referUser);
+
+		Assert.NotNull(referUser);
+		Assert.Equal(referUser, user);
+
+	}
+
+}
